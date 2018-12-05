3b60b510c689baa3c95a59c1f36945f39d33c976
Gaurang <gaurangsaini@gmail.com>
2018-12-05 14:22:59 -0500

    Added Relay Trigger thru GPIO 26  on Wiiboard front button press

--- boot.py
+++ boot.py
@@ -26,5 +26,14 @@ while True:
     input_state = GPIO.input(21)
     if input_state == False:
         print('Button Pressed')
+        
+        GPIO.setup(26, GPIO.OUT)
+        GPIO.output(26, GPIO.HIGH)
+
+        time.sleep(0.25)
+
+        GPIO.output(26, GPIO.LOW)
+        GPIO.cleanup()
+        
         os.system('python /home/pi/SmartScale/posttophp.py 34:AF:2C:2D:9E:4B')
         time.sleep(1)
