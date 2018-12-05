d437d43e262b99b057642664e932a0a508834d1f
Gaurang Saini - SS-2 <gaurangsaini@gmail.com>
2018-12-05 11:26:39 -0500

    Updated scale 2 hardware, and mac address for bluetooth connection accordingly

--- boot.py
+++ boot.py
@@ -26,5 +26,5 @@ while True:
     input_state = GPIO.input(27)
     if input_state == False:
         print('Button Pressed')
-        os.system('python /home/pi/SmartScale/posttophp.py 34:AF:2C:2E:76:00')
+        os.system('python /home/pi/SmartScale/posttophp.py 34:AF:2C:2D:9E:4B')
         time.sleep(1)
